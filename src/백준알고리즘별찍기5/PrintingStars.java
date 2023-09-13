8103d0cfc45311a1cbd431ca99c57cc9b14f0ba9
lastofyears21 <lastofyears21@gmail.com>
2023-09-13 19:11:27 +0900

    23년_09_13 깃 푸시 재 확인

--- src/백준알고리즘별찍기5/PrintingStars.java
+++ src/백준알고리즘별찍기5/PrintingStars.java
@@ -8,7 +8,7 @@ public class PrintingStars {
         int n = sc.nextInt();
 
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <=n - i; j++) {
+            for (int j = 1; j <=n-i; j++) {
                 System.out.print(" ");
             }
 
